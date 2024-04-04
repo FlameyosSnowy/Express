e27e3afe7c4d8d7df78411ba6916344510b736df
FlameyosFlow <89988527+FlameyosSnowy@users.noreply.github.com>
2024-04-04 16:42:15 +0200

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("java")
+}
+
+group = "me.flame.expressive"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("org.projectlombok:lombok:1.18.22")
+    annotationProcessor("org.projectlombok:lombok:1.18.22")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
